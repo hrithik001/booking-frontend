a675b14e83a9b141646634ef828fdff373d9a181
itsmehrithikroshan@gmail.com <itsmehrithikroshan@gmail.com>
2024-07-15 15:13:16 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ import Layout from './layout'
 import RegisterPage from './pages/RegisterPage'
 import axios from 'axios'
 import {UserContextProvider} from './context/UserContext.jsx'
-import AccountPage from './pages/AccountPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 import PlacesPage from './pages/PlacesPage.jsx'
+import AddNewPlaceForm from './pages/AddNewPlaceForm.jsx'
+
 
 
 axios.defaults.baseURL = 'http://localhost:4000'
@@ -26,8 +28,10 @@ function App() {
         <Route index element={<IndexPage/>}></Route>
         <Route path='/login' element={<LoginPage/>}></Route>
         <Route path='/register' element={<RegisterPage/>}></Route>
-        <Route path='/account/:subpage?' element={<AccountPage/>}></Route>
-        <Route path='/account/:subpage/:action' element={<AccountPage/>}></Route>
+        <Route path='/account' element={<ProfilePage/>}></Route>
+        <Route path='/account/places' element={<PlacesPage/>}></Route>
+        <Route path='/account/places/new' element={<AddNewPlaceForm/>}></Route>
+        <Route path='/account/places/:id' element={<AddNewPlaceForm/>}></Route>
        
       </Route>
     </Routes>
