965c54be8f9945611d76cd4ca20dc93f917a2b7a
itsmehrithikroshan@gmail.com <itsmehrithikroshan@gmail.com>
2024-07-15 15:20:47 +0530

    code cleanup , created a new components  ( AccountNav , AddNewPlaceForm ) and updated AccountPage to ProfilePage

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -1,122 +1,55 @@
-import { useState } from "react";
-import { Link ,Navigate,useParams } from "react-router-dom";
-import Perks from "../components/Perks";
-import PhotoUploader from "../components/PhotoUploader";
+
+import { Link  } from "react-router-dom";
+import AccountNav from "../components/AccountNav";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const PlacesPage = () => {
 
-    const {action} = useParams();
-    const [title,setTitle] = useState('');
-    const [description,setDescription] = useState('');
-    const [address,setAddress] = useState('');
-    const [addedphotos ,setAddedPhotos] = useState([]);
-    const [perks,setPerks] = useState([]);
-    const [extraInfo,setExtraInfo] = useState('');
-    const [checkIn,setCheckIn] = useState('');
-    const [checkOut,setCheckOut] = useState('');
-    const [maxGuests,setMaxGuests] = useState(1);
-    const [redirect,setRedirect] = useState('');
-
-
-   const addNewPlace = async (e) => {
-    console.log("photos",addedphotos)
-    e.preventDefault();
-    // setRedirect('')
-    try {
-        const {data} = await axios.post('/place/upload/new',{
-            title,description,address,
-            addedphotos,perks,extraInfo,
-            checkIn,checkOut,maxGuests
+    const [places,setPlaces] = useState([]);
+    useEffect( () => {
+        axios.get('/places').then(({data}) => {
+          setPlaces(data);
         })
-
-        setRedirect('/account/places');
-
-        console.log(data,"successfully added")
-    } catch (error) {
-        console.log(error);
-    }
-   }
-
-   if(redirect)
-   {
-    return <Navigate to={redirect} />
-   }
-
+    })  
 
     return (
       <div>
-         {
-           action !== 'new' && (
-                            <div className="text-center mt-10">
+        <AccountNav/>
+         <div className="text-center mt-10">
 
-                                <Link to={'new'} className="inline-flex bg-primary text-white py-3 px-6 rounded-full">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                    <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
-                                    </svg>
-                                    Add Places
-                                </Link>
+            <Link to={'new'} className="inline-flex bg-primary text-white py-3 px-6 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+                </svg>
+                Add Places
+            </Link>
+            <br />
+            <div className="mt-4">
+              {
+                places.length > 0 && places.map((place,index) => (
+                  <Link to={'/account/places/'+ place._id} className="flex bg-gray-100 p-4 rounded-2xl gap-4 cursor-pointer my-4" key={place._id}>
+                    <div className="w-32 h-32 flex  bg-gray-300 grow-0 shrink-0">
+                        {
+                          place.photos?.length > 0 && (
+                         
+                            <img className="object-cover" src={`http://localhost:4000/uploads/${place.photos[0]}`} alt={place.photos[0]} />
+                          )
+                        }
+                    </div>
+                    <div className="grow-0 shrink">
+                        <h2 className="text-xl ">
+                          {place.title}
+                        </h2>
+                          
+                        <p className="text-sm mt-2">{place.description   }{ console.log(place.photos?.length)}</p>
+                    </div>
+                  </Link>
+                ))
+              }
+            </div>
+         </div>
 
-                            </div>
-        
-                               ) 
-         }
-         {
-            action === 'new' && (
-               <div>
-                    <form onSubmit={addNewPlace}>
-                        <label className="text-2xl mt-4">Name of your Home</label>
-                       
-                        <input 
-                        type="text" 
-                        placeholder="My Home"
-                        value={title}
-                        onChange={(e)=>setTitle(e.target.value)}
-
-                        />
-                        <label className="text-2xl mt-4">Address</label>
-                        <input type="text" placeholder="Address"  value={address}
-                        onChange={(e)=>setAddress(e.target.value)} />
-                        <label className="text-2xl mt-4">Photos</label>
-                        <PhotoUploader addedphotos={addedphotos} onChange={setAddedPhotos} />
-                         <label className="text-2xl mt-6">Description</label>
-                        <textarea placeholder="A great place to visit..."  value={description}
-                        onChange={(e)=>setDescription(e.target.value)}></textarea>
-                        <label className="text-2xl mt-6">Perks</label>
-                        <p className="text-gray-500 text-sm">Select all perks</p>
-                        <div className="grid gris-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 my-4">
-                            <Perks selected={perks} onChange={setPerks}/>
-                        </div>
-                        <label className="text-2xl ">Other Rule's</label>
-                        <textarea  value={extraInfo}
-                        onChange={(e)=>setExtraInfo(e.target.value)}></textarea>
-                        <label className="text-2xl ">Check in&out time</label>
-                         <p className="text-gray-500 text-sm">Adjust the time for cleaning service's</p>
-                        <div className="gap-2 my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                            <div>
-                                    <h3>Check in time</h3>
-                                    <input type="text" placeholder="10:00"  value={checkIn}
-                            onChange={(e)=>setCheckIn(e.target.value)}/>
-                                </div>
-                                <div>
-                                    <h3>Check out time</h3>
-                                    <input type="text" placeholder="10:00"  value={checkOut}
-                            onChange={(e)=>setCheckOut(e.target.value)}/>
-                                </div>
-                                <div>
-                                    <h3>Max number of guests</h3>
-                                    <input type="number" placeholder="10"  value={maxGuests}
-                            onChange={(e)=>setMaxGuests(e.target.value)}/>
-                                </div>
-                        </div>
-
-                        <button className="primary">Submit</button>
-                    </form>
-               </div>
-
-            )
-
-         }
       </div>
     );
 }
